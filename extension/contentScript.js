f4a5def3c11c348fbad9bc9d1921e879bf9c9bee
eaudran <nevosis@gmail.com>
2020-01-12 11:39:38 +0100

    Add jquery to project. Update properly

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,9 +1,4 @@
-// test sur
-let elements = document.getElementsByClassName("title");
-console.log(elements);
-if (elements && elements.length && elements[0]) {
-	for (let i = 0; i < elements.length; i++) {
-		//elements[i].innerHTML = "HAHA";
-		elements[i].style.fontFamily = "Comic Sans MS";
-	}
-}
+setTimeout(function() { // Timeout used because page may complete rendering after extension is loaded
+	$(".title").css("font-size", "15px");
+	$(".title").css("font-family", '"Comic Sans MS"');
+}, 20);
