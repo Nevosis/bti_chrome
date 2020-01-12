3501947a0ce44236467c4c36af6b0d93c3dbd803
eaudran <nevosis@gmail.com>
2020-01-12 11:21:01 +0100

    add todo file.

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -3,7 +3,7 @@ let elements = document.getElementsByClassName("title");
 console.log(elements);
 if (elements && elements.length && elements[0]) {
 	for (let i = 0; i < elements.length; i++) {
-		elements[i].innerHTML = "HAHA";
+		//elements[i].innerHTML = "HAHA";
 		elements[i].style.fontFamily = "Comic Sans MS";
 	}
 }
