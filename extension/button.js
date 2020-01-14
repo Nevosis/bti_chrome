c5f4b44b0c389109fead40b103bd66a00dbc33b9
eaudran <nevosis@gmail.com>
2020-01-14 09:31:12 +0100

    Add style as css to contentscripts

---
+++ extension/button.js
@@ -0,0 +1,4 @@
+// Append button extension style to head !
+//$("<style type='text/css'> .btn-balancetoninfluence{ color:#f00; font-weight:bold;} </style>").appendTo("head");
+
+let button = $("<div class='btn-balancetoninfluence'>mao</div>");
--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,4 +1,11 @@
-setTimeout(function() { // Timeout used because page may complete rendering after extension is loaded
+
+
+setTimeout(function() {
+	// Timeout used because page may complete rendering after extension is loaded
 	$(".title").css("font-size", "15px");
 	$(".title").css("font-family", '"Comic Sans MS"');
+
+	let r = $('<paper-button	noink=""	class="style-scope ytd-subscribe-button-renderer"	role="button"	tabindex="0"	animated=""	elevation="0"	aria-disabled="false"	aria-label="S\'abonner à Kyle Robinson Young. 26&nbsp;k&nbsp;abonnés"	><!--css-build:shady-->	<yt-formatted-string class="style-scope ytd-subscribe-button-renderer"		>S\'abonner</yt-formatted-string></paper-button>');
+	//$(".title").append(r);
+	$(".title").append(button);
 }, 20);
