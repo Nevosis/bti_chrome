2fdcca02c3b6105efd1ba73f610069501cd5e44e
eaudran <nevosis@gmail.com>
2020-01-15 19:13:14 +0100

    Extension now has a report button next to subscribe button. Extension get channel name and ID properly

--- extension/button.js
+++ extension/button.js
@@ -1,4 +1,1 @@
-// Append button extension style to head !
-//$("<style type='text/css'> .btn-balancetoninfluence{ color:#f00; font-weight:bold;} </style>").appendTo("head");
-
-let button = $("<button class='btn-balancetoninfluence'>mao</button>");
+let button = $("<button id='btn-balancetoninfluence' class='btn-balancetoninfluence'>REPORT</button>");
--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,20 +1,43 @@
-function initContentScript() {
-	updateDom();
-	return;
-	// Add clickable button to title
-	$(".title .ytd-video-primary-info-renderer").append(button);
+var justInCase = 10;
+
+function initClick() {
 	$(".btn-balancetoninfluence").click(function() {
-		alert("Handler for .click() called.");
+		console.log("Report called");
+
+		let channelNode = $("#channel-name > div > div > #text > a");
+		if (channelNode && channelNode.length) {
+			let channelName = channelNode[0].innerText 
+			let channelUrl = channelNode[0].href
+			alert(`REPORTING DE :\n${channelName}\n${channelUrl}`);
+			console.log(channelNode[0].innerText);
+			console.log(channelNode[0].href);
+		}
 	});
 }
 
+function initContentScript() {
+	console.log("initContentScript")
+	updateDom();
+
+	let bloc = $("#container > #top-row");
+	if (bloc && bloc[0]) {
+		bloc.append(button);
+		initClick();
+		justInCase = 10;
+	} else {
+		justInCase--;
+		if (justInCase > 0) {
+			setTimeout(initContentScript, 500);
+		}
+	}
+}
+
+// overide css of existing dom element
 function updateDom() {
-	// overide css of existing dom element
 	$(".title .ytd-video-primary-info-renderer").css(
 		"font-family",
 		'"Comic Sans MS"'
 	);
-
 }
 
 // Trigger script on tab update / change url
@@ -25,8 +48,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		request.url.includes("youtube.com/watch?")
 	) {
 		updateDom();
+
+		let bloc = $("#btn-balancetoninfluence");
+		if (!(bloc && bloc[0])) {
+			initContentScript();
+		}
+		return;
 	}
 });
 
-// Timeout used because page may complete rendering after extension is loaded
-setTimeout(initContentScript, 50);
+initContentScript();
